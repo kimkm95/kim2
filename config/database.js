aba8fbdb82a86c67e30b0f1daa804f6942a56230
kimkm95 <74487435+kimkm95@users.noreply.github.com>
2021-12-06 17:38:13 +0900

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+const {logger} = require('./winston');
+
+// TODO: 본인의 DB 계정 입력
+const pool = mysql.createPool({
+    host: 'softsquareddb.c30jwxmcu9gi.ap-northeast-2.rds.amazonaws.com',
+    user: 'master',
+    port: '3306',
+    password: 'Rlarudals12!',
+    database: 'DiaryDB'
+});
+
+module.exports = {
+    pool: pool
+};
